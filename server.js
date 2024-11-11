710d6a3347ff98fc5056abab809dfb99321b5635
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-11-11 11:37:04 -0600

    Initial commit of refactor

--- server.js
+++ server.js
@@ -1,41 +1,17 @@
 'use strict';
 
-require('dotenv').config(); // Allows for use of environment variables
-const express = require('express'); // Framework to manage server and gives functionality to create routes for API
-const cors = require('cors'); // Security for requestes, controlled access to resources
-const getWeather = require('./modules/weather');
-const getMovies = require('./modules/movies');
-const app = express(); // Initialize express
-const PORT = process.env.PORT || 3002; // Use port from .env file
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
-app.use(cors()); // Use express with cors
+const app = express(); // Initiate express
 
+app.use(cors()); // enable cross origin from different sources
 
-// Initial default route to ensure connection is working fine (do this first)
-app.get('/', (request, response, next) => {
+const PORT = process.env.PORT || 3001;
 
-  // Message to send back, connection OK
-  response.status(200).send('Default Route Working');
-
+app.get('/', (request, response) => {
+  response.send('hello from the home route!');
 });
-
-
-// API call to Weatherbit
-app.get('/weather', getWeather);
-
-
-// API call to movieDB
-app.get('/movies', getMovies);
-
-
-// Error handler, inboked any time 'throw new Error' encountered in code
-app.use( (error, request, response, next) => {
-
-  // response.status(500).send(error.message);
-  console.error(error); // Log the error to the server console for debugging
-  response.status(500).json({ error: error.message }); // Send a JSON response with a 500 status and an error message
-
-});
-
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
