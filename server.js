1ee542d87e50bb17f4a09c9a45a9adb2afe42a76
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-11-11 12:25:29 -0600

    Retrieve weather day by cityname latitude and longitude

--- server.js
+++ server.js
@@ -16,7 +16,30 @@ app.get('/', (request, response) => {
 });
 
 app.get('/weather', (request, response) => {
-  response.send('hello from the weather route!');
+  let weatherDataArray = [];
+  weatherDataArray.push(weatherData);
+  console.log('Weather Data Array: ', weatherDataArray);
+
+  let cityName = request.query.cityName;
+  console.log('City Name from request: ', cityName);
+
+  let latitude = parseFloat(request.query.lat);
+  latitude = Math.round(latitude * 1000) / 1000;
+  console.log('Latitude from request: ', latitude);
+
+  let longitude = parseFloat(request.query.lon);
+  longitude = Math.round(longitude * 1000) / 1000;
+  console.log('Longitude from request: ', longitude);
+
+  let cityWeatherData = weatherDataArray.filter((city) => {
+    city.lat = Math.round(city.lat * 1000) / 1000;
+    city.lon = Math.round(city.lon * 1000) / 1000;
+    return city.city_name.toLowerCase() === cityName.toLowerCase() &&
+           city.lat === latitude &&
+           city.lon === longitude;
+  });
+
+  response.send(cityWeatherData[0]);
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
