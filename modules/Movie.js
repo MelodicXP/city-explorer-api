447c6a791ce67d5aa8d323062fa3b8094132a8c4
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-11-16 08:39:36 -0600

    Send proper image url to client

--- modules/Movie.js
+++ modules/Movie.js
@@ -86,7 +86,7 @@ class Movie {
     this.overview = obj.overview;
     this.voteAverage = obj.vote_average;
     this.voteCount = obj.vote_count;
-    this.imageURL = obj.poster_path;
+    this.imageURL = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${obj.poster_path}`;
     this.popularity = obj.popularity;
     this.releaseDate = obj.release_date;
   }
